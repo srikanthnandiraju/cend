a2583fa1120734e40f673a9d9ce166bcacfb8ebc
Srikanth <nandiraju@yahoo.com>
2018-10-15 23:30:02 -0700

    params

--- diwali/f1/js/index.js
+++ diwali/f1/js/index.js
@@ -2353,4 +2353,29 @@ if (IS_HEADER) {
 //     toggleFullscreen();
 // }, 1000);
 
-// toggleSound(true);
+//toggleSound(true);
+
+
+function handleDomContentLoaded(event) {
+    var params = getUrlParams();
+    var from = params.from;
+    // var message = params.message;
+    console.log("From " + from);
+    if (from != undefined)
+        document.querySelector('#from-user').innerHTML = "- " + from;
+}
+
+function getUrlParams(prop) {
+    var params = {};
+    var search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 1));
+    var definitions = search.split('&');
+    definitions.forEach(function(val, key) {
+        var parts = val.split('=', 2);
+        params[parts[0]] = parts[1];
+    });
+    return (prop && prop in params) ? params[prop] : params;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    handleDomContentLoaded();
+});
