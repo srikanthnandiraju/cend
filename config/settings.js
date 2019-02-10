aed7b508c1f2312e39ad306fa23f6a7ba58df69e
Positive Paul <menubooklet@gmail.com>
2019-02-09 23:53:46 -0800

    configs

---
+++ config/settings.js
@@ -0,0 +1,50 @@
+Cards.types = [{
+        id: 1,
+        category: "birthday",
+        name: "Birthday 1",
+        url: "http://cend.in/v1/birthday/card_1.html"
+    },
+    {
+        id: 2,
+        category: "birthday",
+        name: "Birthday 2",
+        name: "Birthday Card 1",
+        url: "http://cend.in/v1/birthday/card_2.html"
+    },
+    {
+        id: 3,
+        category: "birthday",
+        name: "Birthday 3",
+        url: "http://cend.in/v1/birthday/card_3.html"
+    },
+    {
+        id: 4,
+        category: "birthday",
+        name: "Birthday 4",
+        url: "http://cend.in/v1/birthday/card_4.html"
+    },
+    {
+        id: 5,
+        category: "birthday",
+        name: "Birthday Card 5",
+        url: "http://cend.in/v1/birthday/card_5.html"
+    },
+    {
+        id: 6,
+        category: "diwali",
+        name: "Diwali",
+        url: "http://cend.in/diwali/f1/"
+    },
+    {
+        id: 7,
+        category: "announce",
+        name: "Announce 1",
+        url: "http://cend.in/v1/announce/card_1.html"
+    },
+    {
+        id: 8,
+        category: "announce",
+        name: "Announce 2",
+        url: "http://cend.in/v1/announce/card_2.html"
+    }
+];
