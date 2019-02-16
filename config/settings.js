e85d8afec0aaeab35b8c351d852ec1879ba10990
Positive Paul <menubooklet@gmail.com>
2019-02-15 22:25:27 -0800

    some stuff

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,8 @@
 Cards.types = [{
         id: 1,
         category: "birthday",
-        name: "Birthday 1",
+        name: "Baloon Bunny card",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/birthday/card_1.html",
         image: "http://cend.in/img/birthday_1.png"
     },
@@ -9,7 +10,7 @@ Cards.types = [{
         id: 2,
         category: "birthday",
         name: "Birthday 2",
-        name: "Birthday Card 1",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/birthday/card_2.html",
         image: "http://cend.in/img/birthday_2.png"
     },
@@ -17,6 +18,7 @@ Cards.types = [{
         id: 3,
         category: "birthday",
         name: "Birthday 3",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/birthday/card_3.html",
         image: "http://cend.in/img/birthday_3.png"
     },
@@ -24,6 +26,7 @@ Cards.types = [{
         id: 4,
         category: "birthday",
         name: "Birthday 4",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/birthday/card_4.html",
         image: "http://cend.in/img/birthday_4.png"
     },
@@ -31,13 +34,15 @@ Cards.types = [{
         id: 5,
         category: "birthday",
         name: "Birthday Card 5",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/birthday/card_5.html",
         image: "http://cend.in/img/birthday_5.png"
     },
     {
         id: 6,
-        category: "diwali",
-        name: "Diwali",
+        category: "fireworks",
+        name: "Exploding Fireworks",
+        description: "spectacular fireworks with sound",
         url: "http://cend.in/diwali/f1/",
         image: "http://cend.in/img/fireworks_1.png"
     },
@@ -45,6 +50,7 @@ Cards.types = [{
         id: 7,
         category: "announce",
         name: "Announce 1",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/announce/card_1.html",
         image: "http://cend.in/img/announce_1.png"
     },
@@ -52,6 +58,7 @@ Cards.types = [{
         id: 8,
         category: "announce",
         name: "Announce 2",
+        description: "A colorful card with cartoon animation",
         url: "http://cend.in/v1/announce/card_2.html",
         image: "http://cend.in/img/announce_2.png"
     }
