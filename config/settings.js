4ffcf0ca4c0de91fc010208d76a6460a5842ad4b
Positive Paul <menubooklet@gmail.com>
2019-02-15 18:00:55 -0800

    updated

--- config/settings.js
+++ config/settings.js
@@ -10,42 +10,49 @@ Cards.types = [{
         category: "birthday",
         name: "Birthday 2",
         name: "Birthday Card 1",
-        url: "http://cend.in/v1/birthday/card_2.html"
+        url: "http://cend.in/v1/birthday/card_2.html",
+        image: "http://cend.in/img/birthday_2.png"
     },
     {
         id: 3,
         category: "birthday",
         name: "Birthday 3",
-        url: "http://cend.in/v1/birthday/card_3.html"
+        url: "http://cend.in/v1/birthday/card_3.html",
+        image: "http://cend.in/img/birthday_3.png"
     },
     {
         id: 4,
         category: "birthday",
         name: "Birthday 4",
-        url: "http://cend.in/v1/birthday/card_4.html"
+        url: "http://cend.in/v1/birthday/card_4.html",
+        image: "http://cend.in/img/birthday_4.png"
     },
     {
         id: 5,
         category: "birthday",
         name: "Birthday Card 5",
-        url: "http://cend.in/v1/birthday/card_5.html"
+        url: "http://cend.in/v1/birthday/card_5.html",
+        image: "http://cend.in/img/birthday_5.png"
     },
     {
         id: 6,
         category: "diwali",
         name: "Diwali",
-        url: "http://cend.in/diwali/f1/"
+        url: "http://cend.in/diwali/f1/",
+        image: "http://cend.in/img/fireworks_1.png"
     },
     {
         id: 7,
         category: "announce",
         name: "Announce 1",
-        url: "http://cend.in/v1/announce/card_1.html"
+        url: "http://cend.in/v1/announce/card_1.html",
+        image: "http://cend.in/img/announce_1.png"
     },
     {
         id: 8,
         category: "announce",
         name: "Announce 2",
-        url: "http://cend.in/v1/announce/card_2.html"
+        url: "http://cend.in/v1/announce/card_2.html",
+        image: "http://cend.in/img/announce_2.png"
     }
 ];
